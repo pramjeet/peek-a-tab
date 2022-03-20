bb0166f6647f82ec7c8c72572dea9cebcdb7fefe
敖武 <dongdong.fdd@alibaba-inc.com>
2022-03-20 15:10:56 +0800

    fix: eslint

--- app.js
+++ app.js
@@ -2,60 +2,54 @@
  * Created by fudongdong on 2021-11-13
  */
 
-document.addEventListener("DOMContentLoaded", function() {
-  var activeWindowId = window.location.search.substr(1);
-  var peekATabWindowId = null;
-  var closeOnFocusChange = true;
+document.addEventListener('DOMContentLoaded', () => {
+  let activeWindowId = window.location.search.substr(1);
+  let peekATabWindowId = null;
+  let closeOnFocusChange = true;
 
-  var activeTabElement = null;
+  let activeTabElement = null;
 
-  var mouseBehavior = "click"; //click, hover or single-click
+  let mouseBehavior = 'click'; // click, hover or single-click
 
-  var tabsListEl = document.getElementById("tabs-list");
-  var searchInput = document.getElementById("search-input");
+  const tabsListEl = document.getElementById('tabs-list');
+  const searchInput = document.getElementById('search-input');
 
   // searchInput.focus()
-  searchInput.addEventListener("input", function() {
+  searchInput.addEventListener('input', () => {
     populateTabs();
   });
-  //save width changes
-  window.addEventListener("resize", function(event) {
-    var widthToStore = document.documentElement.clientWidth;
+  // save width changes
+  window.addEventListener('resize', (event) => {
+    let widthToStore = document.documentElement.clientWidth;
     if (widthToStore <= 300) {
       widthToStore = 300;
     }
 
     chrome.storage.sync.set({
-      windowWidth: widthToStore
+      windowWidth: widthToStore,
     });
   });
 
   function highlight(text, textToHighlight) {
     if (textToHighlight.length > 0) {
-      var keywords = [getChineseKeyWordsByPinyin(text,textToHighlight)].concat(textToHighlight).filter(function(str){
-        return str != null && str.trim() != '';
-      });
-      console.info('keywords is ',keywords)
-      var regex = new RegExp(keywords.join('|'),'g')
-      console.info('regex and text is ',regex,text)
+      const keywords = [getChineseKeyWordsByPinyin(text, textToHighlight)].concat(textToHighlight).filter((str) => str != null && str.trim() != '');
+      console.info('keywords is ', keywords);
+      const regex = new RegExp(keywords.join('|'), 'g');
+      console.info('regex and text is ', regex, text);
       return text.toLowerCase()
-        .replace(regex,function(kw){
-          return "<span class='highlight'>" + kw + "</span>"
-        })
+        .replace(regex, (kw) => `<span class='highlight'>${kw}</span>`);
     }
-    else return text;
+    return text;
   }
 
-  function getChineseKeyWordsByPinyin(text,pinyin){
-    var chars = text.split('');
-    var pinyins = chars.map(function(char){
-      return pinyinUtil.getPinyin(char);
-    })
-    var keywords = [];
-    for(var i = 0;i<pinyins.length;i++){
-      var _char = chars[i];
-      var _pinyin = pinyins[i]
-      if(pinyin.indexOf(_pinyin) >= 0 && _char != _pinyin && _char.trim() != ''){
+  function getChineseKeyWordsByPinyin(text, pinyin) {
+    const chars = text.split('');
+    const pinyins = chars.map((char) => pinyinUtil.getPinyin(char));
+    const keywords = [];
+    for (let i = 0; i < pinyins.length; i++) {
+      const _char = chars[i];
+      const _pinyin = pinyins[i];
+      if (pinyin.indexOf(_pinyin) >= 0 && _char != _pinyin && _char.trim() != '') {
         keywords.push(_char);
       }
     }
@@ -67,11 +61,11 @@ document.addEventListener("DOMContentLoaded", function() {
    * @param tabElement - DOM element from the list containing data-id attributes
    */
   function removeTabElement(tabElement) {
-    var tabId = tabElement.dataset.id;
+    const tabId = tabElement.dataset.id;
 
-    var tabEls = document.getElementsByClassName("tab");
+    const tabEls = document.getElementsByClassName('tab');
     if (tabElement == activeTabElement) {
-      for (var i = 0; i < tabEls.length; i++) {
+      for (let i = 0; i < tabEls.length; i++) {
         if (tabId == tabEls[i].id) {
           if (i == tabEls.length - 1) {
             makePreviousTabActive();
@@ -88,20 +82,20 @@ document.addEventListener("DOMContentLoaded", function() {
     chrome.tabs.remove(+tabId);
     tabElement.remove();
 
-    //if only one tab was present before closing this tab, close tabs-manager window too
+    // if only one tab was present before closing this tab, close tabs-manager window too
     if (tabEls.length == 1) {
       window.close();
     }
   }
 
-  //get tabs-manager window id and listener for focus change
-  chrome.windows.getCurrent({}, function(peekATabWindow) {
+  // get tabs-manager window id and listener for focus change
+  chrome.windows.getCurrent({}, (peekATabWindow) => {
     peekATabWindowId = peekATabWindow.id;
-    chrome.windows.onFocusChanged.addListener(function(newWindowId) {
+    chrome.windows.onFocusChanged.addListener((newWindowId) => {
       if (
-        closeOnFocusChange &&
-        newWindowId != peekATabWindow.id &&
-        newWindowId != chrome.windows.WINDOW_ID_NONE
+        closeOnFocusChange
+        && newWindowId != peekATabWindow.id
+        && newWindowId != chrome.windows.WINDOW_ID_NONE
       ) {
         window.close();
       }
@@ -113,12 +107,12 @@ document.addEventListener("DOMContentLoaded", function() {
    * @param {*} tab
    */
   function changeActiveTab(tab) {
-    //if tab is in different window than change focus to that that window first
+    // if tab is in different window than change focus to that that window first
     if (tab.windowId != activeWindowId) {
       closeOnFocusChange = false;
       changeActiveWindowTitle(activeWindowId, tab.windowId);
-      chrome.windows.update(tab.windowId, { focused: true }, function() {
-        chrome.windows.update(peekATabWindowId, { focused: true }, function() {
+      chrome.windows.update(tab.windowId, { focused: true }, () => {
+        chrome.windows.update(peekATabWindowId, { focused: true }, () => {
           closeOnFocusChange = true;
         });
       });
@@ -127,11 +121,11 @@ document.addEventListener("DOMContentLoaded", function() {
     chrome.tabs.update(tab.id, { active: true });
     activeWindowId = tab.windowId;
 
-    if (activeTabElement) activeTabElement.classList.remove("active");
+    if (activeTabElement) activeTabElement.classList.remove('active');
 
     activeTabElement = document.getElementById(tab.id);
 
-    activeTabElement.classList.add("active");
+    activeTabElement.classList.add('active');
     if (document.activeElement != searchInput) {
       activeTabElement.focus();
     }
@@ -139,10 +133,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function changeActiveWindowTitle(oldWinId, newWinId) {
     oldWin = document.getElementById(oldWinId);
-    oldWin.textContent = "窗口 " + oldWin.dataset.nr;
+    oldWin.textContent = `窗口 ${oldWin.dataset.nr}`;
 
     newWin = document.getElementById(newWinId);
-    newWin.textContent = "窗口 " + newWin.dataset.nr + " (当前窗口)";
+    newWin.textContent = `窗口 ${newWin.dataset.nr} (当前窗口)`;
   }
 
   function changeActiveTabAndCloseWindow(tab) {
@@ -151,13 +145,13 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function hoveredOnTab(tab) {
-    if (mouseBehavior == "hover") {
+    if (mouseBehavior == 'hover') {
       changeActiveTab(tab);
     }
   }
 
   function clickedOnTab(tab) {
-    if (mouseBehavior == "click") {
+    if (mouseBehavior == 'click') {
       changeActiveTab(tab);
     } else {
       changeActiveTabAndCloseWindow(tab);
@@ -169,24 +163,23 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function getActiveTabIndex() {
-    var tabIndex;
-    var tabEls = document.getElementsByClassName("tab");
+    let tabIndex;
+    const tabEls = document.getElementsByClassName('tab');
 
-    for (tabIndex = 0; tabIndex < tabEls.length; tabIndex++)
-      if (activeTabElement.id == tabEls[tabIndex].id) return tabIndex;
+    for (tabIndex = 0; tabIndex < tabEls.length; tabIndex++) if (activeTabElement.id == tabEls[tabIndex].id) return tabIndex;
   }
 
   function makeTabIndexActive(tabIndex) {
-    var tabElement = document.getElementsByClassName("tab")[tabIndex];
-    var tabId = tabElement.id;
-    chrome.tabs.get(+tabId, function(tab) {
+    const tabElement = document.getElementsByClassName('tab')[tabIndex];
+    const tabId = tabElement.id;
+    chrome.tabs.get(+tabId, (tab) => {
       changeActiveTab(tab);
     });
   }
 
   function makeNextTabActive() {
-    var tabIndex = getActiveTabIndex();
-    var tabEls = document.getElementsByClassName("tab");
+    const tabIndex = getActiveTabIndex();
+    const tabEls = document.getElementsByClassName('tab');
 
     if (tabIndex < tabEls.length - 1) {
       makeTabIndexActive(tabIndex + 1);
@@ -196,8 +189,8 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function makePreviousTabActive() {
-    var tabIndex = getActiveTabIndex();
-    var tabEls = document.getElementsByClassName("tab");
+    const tabIndex = getActiveTabIndex();
+    const tabEls = document.getElementsByClassName('tab');
     if (tabIndex > 0) {
       makeTabIndexActive(tabIndex - 1);
       return;
@@ -210,101 +203,100 @@ document.addEventListener("DOMContentLoaded", function() {
    * get all tabs from all windows and populates the list
    */
   function populateTabs() {
-    chrome.windows.getAll({ populate: true, windowTypes: ["normal"] }, function(
-      windows
-    ) {
-      //if no window present, close tags-manager
+    chrome.windows.getAll({ populate: true, windowTypes: ['normal'] }, (
+      windows,
+    ) => {
+      // if no window present, close tags-manager
       if (windows.length == 0) {
         window.close();
       }
 
-      tabsListEl.innerHTML = "";
+      tabsListEl.innerHTML = '';
 
-      for (var i = 0; i < windows.length; i++) {
-        var aWindow = windows[i];
-        var isActiveWindowText =
-          activeWindowId == aWindow.id ? " (当前窗口)" : "";
-        var windowTitle = document.createElement("li");
-        windowTitle.classList.add("window-text");
+      for (let i = 0; i < windows.length; i++) {
+        const aWindow = windows[i];
+        const isActiveWindowText = activeWindowId == aWindow.id ? ' (当前窗口)' : '';
+        const windowTitle = document.createElement('li');
+        windowTitle.classList.add('window-text');
         windowTitle.id = aWindow.id;
         windowTitle.dataset.nr = i + 1;
-        windowTitle.textContent = "窗口 " + (i + 1) + isActiveWindowText;
+        windowTitle.textContent = `窗口 ${i + 1}${isActiveWindowText}`;
         tabsListEl.appendChild(windowTitle);
 
-        for (var j = 0; j < aWindow.tabs.length; j++) {
-          var tab = aWindow.tabs[j];
+        for (let j = 0; j < aWindow.tabs.length; j++) {
+          const tab = aWindow.tabs[j];
 
           if (
-            searchInput.value.trim() != "" &&
-            tab.title
+            searchInput.value.trim() != ''
+            && tab.title
               .toLowerCase()
-              .indexOf(searchInput.value.trim().toLowerCase()) == -1 &&
-            tab.url
+              .indexOf(searchInput.value.trim().toLowerCase()) == -1
+            && tab.url
               .toLowerCase()
-              .indexOf(searchInput.value.trim().toLowerCase()) == -1 &&
+              .indexOf(searchInput.value.trim().toLowerCase()) == -1
 
             // chinese search
-            pinyinUtil.getPinyin(tab.title)
-              .replace(/\W/g,'')
+            && pinyinUtil.getPinyin(tab.title)
+              .replace(/\W/g, '')
               .indexOf(searchInput.value.trim().toLowerCase()) == -1
           ) {
             continue;
           }
 
-          var tabEl = document.createElement("li");
-          tabEl.classList.add("tab");
+          const tabEl = document.createElement('li');
+          tabEl.classList.add('tab');
           tabEl.dataset.id = tab.id;
           tabEl.tabIndex = 0;
           tabEl.id = tab.id;
 
-          var tabImgEl = document.createElement("img");
-          tabImgEl.classList.add("icon");
-          tabImgEl.src = tab.favIconUrl || "";
+          const tabImgEl = document.createElement('img');
+          tabImgEl.classList.add('icon');
+          tabImgEl.src = tab.favIconUrl || '';
 
-          var tabTitleEl = document.createElement("p");
-          tabTitleEl.classList.add("title");
+          const tabTitleEl = document.createElement('p');
+          tabTitleEl.classList.add('title');
           tabTitleEl.innerHTML = highlight(tab.title, searchInput.value);
 
-          var tabCrossEl = document.createElement("img");
-          tabCrossEl.title = "close";
-          tabCrossEl.classList.add("close-icon");
-          tabCrossEl.src = "images/close_icon.png";
+          const tabCrossEl = document.createElement('img');
+          tabCrossEl.title = 'close';
+          tabCrossEl.classList.add('close-icon');
+          tabCrossEl.src = 'images/close_icon.png';
 
           tabCrossEl.addEventListener(
-            "click",
-            (function(tabElement) {
-              return function(e) {
+            'click',
+            (function (tabElement) {
+              return function (e) {
                 e.stopPropagation();
                 removeTabElement(tabElement);
               };
-            })(tabEl)
+            }(tabEl)),
           );
 
           tabEl.addEventListener(
-            "mouseover",
-            (function(tab, tabElement) {
-              return function() {
+            'mouseover',
+            (function (tab, tabElement) {
+              return function () {
                 hoveredOnTab(tab);
               };
-            })(tab)
+            }(tab)),
           );
 
           tabEl.addEventListener(
-            "click",
-            (function(tab, tabElement) {
-              return function() {
+            'click',
+            (function (tab, tabElement) {
+              return function () {
                 clickedOnTab(tab);
               };
-            })(tab)
+            }(tab)),
           );
 
           tabEl.addEventListener(
-            "dblclick",
-            (function(tab, tabElement) {
-              return function() {
+            'dblclick',
+            (function (tab, tabElement) {
+              return function () {
                 doubleClickedOnTab(tab);
               };
-            })(tab)
+            }(tab)),
           );
 
           tabEl.appendChild(tabImgEl);
@@ -320,42 +312,42 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
-  //key bindings
-  document.onkeydown = function(e) {
+  // key bindings
+  document.onkeydown = function (e) {
     switch (e.keyCode) {
-      case 38: //up arrow
+      case 38: // up arrow
         e.preventDefault();
         searchInput.blur();
         makePreviousTabActive();
         break;
-      case 40: //down arrow
+      case 40: // down arrow
         e.preventDefault();
         searchInput.blur();
         makeNextTabActive();
         break;
-      case 13: //enter
+      case 13: // enter
         // open first tab when where is no actived tab
-        var activeTab = document.querySelector("li.tab.active");
-        if(!activeTab){
-          var arrowDownEvent = new Event('keydown');
+        var activeTab = document.querySelector('li.tab.active');
+        if (!activeTab) {
+          const arrowDownEvent = new Event('keydown');
           arrowDownEvent.keyCode = 40;
-          document.dispatchEvent(arrowDownEvent)
-        }else{
-          window.close()
+          document.dispatchEvent(arrowDownEvent);
+        } else {
+          window.close();
         }
         break;
-      case 27: //esc
+      case 27: // esc
         window.close();
         break;
-      case 8: //backspace
-      case 46: //delete
+      case 8: // backspace
+      case 46: // delete
         if (document.activeElement != searchInput) {
-          //search input is not focused
+          // search input is not focused
           removeTabElement(activeTabElement);
         }
         break;
       default:
-        //if search input is not focused and space or any of alphanumeric keys is pressed
+        // if search input is not focused and space or any of alphanumeric keys is pressed
         if (document.activeElement != searchInput) {
           searchInput.focus();
           // searchInput.value = searchInput.value + String.fromCharCode(e.keyCode)
@@ -364,22 +356,21 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   };
 
-  var mouseBehaviorImage = document.getElementById("mouse-behavior-image");
+  const mouseBehaviorImage = document.getElementById('mouse-behavior-image');
 
   function changeMouseBehaviorImage() {
-    mouseBehaviorImage.src =
-      "./images/cursor-pointer-" + mouseBehavior + ".png";
+    mouseBehaviorImage.src = `./images/cursor-pointer-${mouseBehavior}.png`;
   }
 
   function saveMouseBehavior() {
     chrome.storage.sync.set({
-      mouseBehavior: mouseBehavior
+      mouseBehavior,
     });
   }
 
-  //show hint to change mouse behavior
-  chrome.storage.sync.get(null, function(items) {
-    if (typeof items.mouseBehavior == "undefined") {
+  // show hint to change mouse behavior
+  chrome.storage.sync.get(null, (items) => {
+    if (typeof items.mouseBehavior === 'undefined') {
       saveMouseBehavior();
     } else {
       mouseBehavior = items.mouseBehavior;
@@ -388,43 +379,42 @@ document.addEventListener("DOMContentLoaded", function() {
     changeMouseBehaviorImage();
   });
 
-  var mouseBehaviorOptionsContainer = document.getElementById(
-    "mouse-behavior-options-container"
+  const mouseBehaviorOptionsContainer = document.getElementById(
+    'mouse-behavior-options-container',
   );
 
-  var mouseBehaviorOptions = document.getElementsByClassName(
-    "mouse-behavior-option"
+  const mouseBehaviorOptions = document.getElementsByClassName(
+    'mouse-behavior-option',
   );
 
-  for (var i = 0; i < mouseBehaviorOptions.length; i++) {
+  for (let i = 0; i < mouseBehaviorOptions.length; i++) {
     mouseBehaviorOptions[i].addEventListener(
-      "click",
-      (function(mouseBehaviorOption) {
-        return function() {
+      'click',
+      (function (mouseBehaviorOption) {
+        return function () {
           mouseBehavior = mouseBehaviorOption.dataset.mouseBehavior;
           changeMouseBehaviorImage();
           saveMouseBehavior();
         };
-      })(mouseBehaviorOptions[i])
+      }(mouseBehaviorOptions[i])),
     );
   }
 
-  mouseBehaviorImage.addEventListener("click", function(e) {
+  mouseBehaviorImage.addEventListener('click', (e) => {
     e.stopPropagation();
 
-    if (mouseBehaviorOptionsContainer.style.display != "block") {
-      mouseBehaviorOptionsContainer.style.display = "block";
+    if (mouseBehaviorOptionsContainer.style.display != 'block') {
+      mouseBehaviorOptionsContainer.style.display = 'block';
     } else {
-      mouseBehaviorOptionsContainer.style.display = "none";
+      mouseBehaviorOptionsContainer.style.display = 'none';
     }
   });
 
-  document.addEventListener("click", function() {
-    if (mouseBehaviorOptionsContainer.style.display != "none") {
-      mouseBehaviorOptionsContainer.style.display = "none";
+  document.addEventListener('click', () => {
+    if (mouseBehaviorOptionsContainer.style.display != 'none') {
+      mouseBehaviorOptionsContainer.style.display = 'none';
     }
   });
 
   populateTabs();
-
 });
--- popup.js
+++ popup.js
@@ -1,33 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var popupWindow;
+document.addEventListener('DOMContentLoaded', () => {
+  let popupWindow;
 
-    var browser = chrome;
+  const browser = chrome;
 
-    chrome.storage.sync.get(null, function (items) {
+  chrome.storage.sync.get(null, (items) => {
+    let width = 400;
 
-        var width = 400;
+    if (typeof items.windowWidth === 'number') {
+      width = items.windowWidth;
+    }
 
-        if (typeof items.windowWidth == "number") {
-            width = items.windowWidth;
-        }
+    browser.windows.getCurrent((win) => {
+      browser.tabs.query({ windowId: win.id, active: true }, (tabs) => {
+        const tab = tabs[0];
 
-
-        browser.windows.getCurrent(function (win) {
-            browser.tabs.query({windowId: win.id, active: true}, function (tabs) {
-                var tab = tabs[0];
-                
-                chrome.windows.create({
-					'url': browser.runtime.getURL("app.html") + "?" + win.id,
-					'type': 'popup',
-					'focused': true,
-					'width': width,
-					'height': (win.height - 10),
-					'left': window.screenLeft,
-					'top': (window.screenTop - 70)
-				});
-
-                window.close(); // close the Chrome extension pop-up
-            });
+        chrome.windows.create({
+          url: `${browser.runtime.getURL('app.html')}?${win.id}`,
+          type: 'popup',
+          focused: true,
+          width,
+          height: (win.height - 10),
+          left: window.screenLeft,
+          top: (window.screenTop - 70),
         });
+
+        window.close(); // close the Chrome extension pop-up
+      });
     });
+  });
 }, false);
